4b10335747e40d919cc376190b964c20b6b00297
romamamedov437-sys <romamamedov437@gmail.com>
2025-09-20 16:52:43 +0700

    Delete ai_photostudio directory

--- ai_photostudio/utils/replicate_api.py
+++
@@ -1,8 +0,0 @@
-import replicate
-import os
-
-replicate_api_token = os.getenv("REPLICATE_API_TOKEN")
-client = replicate.Client(api_token=replicate_api_token)
-
-def train_model():
-    return "Training started"
